b1490221571ee5694d18fd9742508d5a0e545df3
Ziyuan Han <hanziyuan08@outlook.com>
2022-11-01 01:00:43 +0800

    feat: Add dependency config-rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
+use std::error::Error;
 use std::process::exit;
 
 use clap::Parser;
+use config::Config;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -14,16 +16,30 @@ struct Cli {
 
 const MAGNET_PREFIX: &str = "magnet:";
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
     peek(&cli);
+    load_config().unwrap();
+    go_qbittorrent().unwrap();
+    Ok(())
+}
 
+fn load_config() -> Result<(), Box<dyn Error>> {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("$HOME/.config/aqu/config"))
+        .build()?;
+
+    // Print out our settings (as a HashMap)
+    println!("{:?}", settings.try_deserialize::<HashMap<String, String>>()?);
+    Ok(())
+}
+
+fn go_qbittorrent() -> Result<(), Box<dyn Error>> {
     let client = reqwest::blocking::Client::new();
     let resp = client.post("http://qbittorrent.example.com/api/v2/auth/login")
         .form(&(("username", "admin"), ("password", "YOUR_PASSWORD")))
         .send()?;
     println!("{:#?}", resp.text()?);
-
     Ok(())
 }
 
