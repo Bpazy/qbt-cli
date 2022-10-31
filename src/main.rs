55b85ce7853958be62e5115f8d2612eefa8af491
Ziyuan Han <hanziyuan08@outlook.com>
2022-10-31 18:03:29 +0800

    chore: Add CLI handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::process::exit;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    verbose: bool,
+
+    uri: Option<String>,
+}
+
+const MAGNET_PREFIX: &str = "magnet:";
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    peek(&cli);
+
+    match cli.uri {
+        None => {
+            println!("magnet uri is required");
+            exit(0)
+        }
+        Some(uri) => {
+            if uri.starts_with(MAGNET_PREFIX) {
+                println!("url: {:?}", uri);
+            } else {
+                println!("magnet uri is invalid");
+            }
+        }
+    }
+}
+
+fn peek(cli: &Cli) {
+    println!("verbose: {:?}", cli);
 }
