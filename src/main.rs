ee82ef1f79edc13dda5b99ee81719c32b5a767a9
Ziyuan Han <hanziyuan08@outlook.com>
2022-11-09 18:15:18 +0800

    list

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use std::error::Error;
 
-use crate::cli::{Add, Cli, Commands};
+use reqwest::blocking::Client;
+
+use crate::cli::{Add, Cli, Commands, List};
 use crate::config::AquConfig;
 
 mod cli;
@@ -8,29 +11,26 @@ mod config;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::load();
+    let aqu_cfg = AquConfig::load();
     match &cli.command {
-        Commands::Add(add) => {
-            add_magnet(AquConfig::load(), add);
+        Commands::Add(add_cmd) => {
+            add_magnet(&aqu_cfg, add_cmd);
+        }
+        Commands::List(list_cmd) => {
+            list(&aqu_cfg, list_cmd);
         }
     }
     Ok(())
 }
 
-fn add_magnet(config: AquConfig, add: &Add) {
-    let client = reqwest::blocking::Client::builder().cookie_store(true).build().unwrap();
-    let resp = client.post(&config.get_login_url())
-        .form(&(("username", &config.username), ("password", &config.password)))
-        .send()
-        .expect(format!("Login failed: {}", &config.get_login_url()).as_str());
-    println!("Login result: {:#?}", resp.text().unwrap());
-
-    let resp = client.post(&config.get_add_torrent_url())
+fn add_magnet(config: &AquConfig, add_cmd: &Add) {
+    let resp = login(&config).post(&config.get_add_torrent_url())
         .form(&(
-            ("urls", &add.uri),
+            ("urls", &add_cmd.uri),
             ("autoTMM", true),
             ("cookie", ""),
-            ("rename", &add.rename),
-            ("category", &add.category),
+            ("rename", &add_cmd.rename),
+            ("category", &add_cmd.category),
             ("paused", "false"),
             ("contentLayout", "Original"),
             ("dlLimit", "NaN"),
@@ -39,4 +39,52 @@ fn add_magnet(config: AquConfig, add: &Add) {
         .send()
         .expect(format!("Add torrent failed {}", &config.get_add_torrent_url()).as_str());
     println!("Add torrent result: {:#?}", resp.text().unwrap());
+}
+
+
+fn list(config: &AquConfig, cmd: &List) {
+    let resp = login(&config).post(&config.get_add_torrent_url())
+        .form(&get_form(&cmd))
+        .send()
+        .expect(format!("Add torrent failed {}", &config.get_add_torrent_url()).as_str());
+    println!("Add torrent result: {:#?}", resp.text().unwrap());
+}
+
+fn get_form(cmd: &List) -> HashMap<&str, String> {
+    let mut form: HashMap<&str, String> = HashMap::new();
+    if let Some(filter) = &cmd.filter {
+        form.insert("filter", filter.to_string());
+    }
+    if let Some(category) = &cmd.category {
+        form.insert("category", category.to_string());
+    }
+    if let Some(tag) = &cmd.tag {
+        form.insert("tag", tag.to_string());
+    }
+    if let Some(sort) = &cmd.sort {
+        form.insert("sort", sort.to_string());
+    }
+    if let Some(reverse) = &cmd.reverse {
+        form.insert("reverse", reverse.to_string());
+    }
+    if let Some(limit) = &cmd.limit {
+        form.insert("limit", limit.to_string());
+    }
+    if let Some(offset) = &cmd.offset {
+        form.insert("offset", offset.to_string());
+    }
+    if let Some(hashes) = &cmd.hashes {
+        form.insert("hashes", hashes.to_string());
+    }
+    form
+}
+
+fn login(config: &&AquConfig) -> Client {
+    let client = Client::builder().cookie_store(true).build().unwrap();
+    let resp = client.post(&config.get_login_url())
+        .form(&(("username", &config.username), ("password", &config.password)))
+        .send()
+        .expect(format!("Login failed: {}", &config.get_login_url()).as_str());
+    println!("Login result: {:#?}", resp.text().unwrap());
+    client
 }
