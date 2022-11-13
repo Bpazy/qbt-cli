5b28284a0325a3f5b2b8f36ddeb47afe08f50ac8
Ziyuan Han <hanziyuan08@outlook.com>
2022-11-13 23:57:03 +0800

    chore: Replace `unwrap()` with `?`

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ mod cli;
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::load();
     if cli.verbose {
-        simple_logger::init_with_level(log::Level::Debug).unwrap();
+        simple_logger::init_with_level(log::Level::Debug)?;
     } else {
-        simple_logger::init_with_level(log::Level::Info).unwrap();
+        simple_logger::init_with_level(log::Level::Info)?;
     }
     let qbt_cfg = QbtConfig::load();
     let qbt_client = QbtClient::login(&qbt_cfg.qbittorrent_host, &qbt_cfg.username, &qbt_cfg.password)?;
