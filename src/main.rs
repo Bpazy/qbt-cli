9467ef1fea61d02c0bd661f24cea9c637937cfac
Ziyuan Han <hanziyuan08@outlook.com>
2022-11-14 16:00:37 +0800

    chore: extract func for setting logger level

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,17 @@ mod cli;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::load();
+    set_logger_level(&cli);
+    let qbt_cfg = QbtConfig::load();
+    let qbt_client = QbtClient::login(&qbt_cfg.qbittorrent_host, &qbt_cfg.username, &qbt_cfg.password)?;
+    cli.command.exec(&qbt_client);
+    Ok(())
+}
+
+fn set_logger_level(cli: &Cli) {
     if cli.verbose {
         simple_logger::init_with_level(log::Level::Debug)?;
     } else {
         simple_logger::init_with_level(log::Level::Info)?;
     }
-    let qbt_cfg = QbtConfig::load();
-    let qbt_client = QbtClient::login(&qbt_cfg.qbittorrent_host, &qbt_cfg.username, &qbt_cfg.password)?;
-    cli.command.exec(&qbt_client);
-    Ok(())
 }
